ad4834327d97d4bf2b13a767df34e0ec1e360ca7
Simone Pachera <simone.pachera@leotech.com.sg>
2017-12-01 16:10:14 +0000

    mock

---
+++ src/mocks/home-page.ts
@@ -0,0 +1,59 @@
+export const HomePageMock = {
+  metrics: [
+    {
+      title: 'Income',
+      amount: 5646,
+      other: ''
+    },
+    {
+      title: 'Outcome',
+      amount: 4808,
+      other: '55% of Income'
+    },
+    {
+      title: 'Account',
+      amount: 802,
+      other: '302 Actual'
+    },
+    {
+      title: 'Credit Card',
+      amount: 502,
+      other: '898 Remaining'
+    }
+  ],
+  savings: [
+    {
+      title: 'Saving 2%',
+      amount: 500,
+      other: ''
+    },
+    {
+      title: 'Saving 3%',
+      amount: 1200,
+      other: ''
+    },
+    {
+      title: 'Savings',
+      amount: 1700,
+      other: '2002 Total'
+    }
+  ],
+  categories: [
+    {
+      title: 'Utenze',
+      amount: 1509,
+      other: '31.39%'
+    },
+    {
+      title: 'Montly',
+      amount: 65,
+      other: '31.39%'
+    },
+    {
+      title: 'Tot Others',
+      amount: 2057,
+      other: '31.39%'
+    }
+  ]
+};
+
